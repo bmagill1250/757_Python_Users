fdd3e45c524a4714b720d1c1533c533303ff6b1d
bmagill1250 <Snake$1986Friend>
2015-03-29 20:57:42 -0400

    Filled out many of the slides for the presentation
    Sinc plot added. Also code for all examples

--- April_2015/example_CPP_STL/vector_string.pyx
+++ April_2015/example_CPP_STL/vector_string.pyx
@@ -5,9 +5,10 @@ def page():
     
     cdef vector[string] content
     
-    content.push_back("Three Rings for the elven kings")
-    content.push_back("Seven for the dwarf lords")
-    content.push_back("Nine for the mortal men")
-    content.push_back("one for the dark lord")
-    
+    content.push_back("This thing all things devours:")
+    content.push_back("Birds, beasts, trees, flowers;")
+    content.push_back("Gnaws iron, bites steel;")
+    content.push_back("Grinds hard stones to meal;")
+    content.push_back("Slays king, ruins town,:)
+    content.push_back("And beats high mountain down:)
     return content
---
+++ April_2015/kruft/func_defs.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 22 18:58:34 2015
+
+@author: brianmagill
+"""
+
+from math import sin, pi
+
+def sinc(x):
+    
+    if (x == 0.0):
+    
+    	return 1.0
+     
+    else:
+    
+    	return sin(pi*x)/(pi*x)
